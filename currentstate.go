20de46a2708f4118d9aa08159619d6f2f29011c5
Gregory Vincic <g@7de.se>
2020-12-11 13:54:03 +0100

    Rename excel to spreadsheet

--- currentstate.go
+++ currentstate.go
@@ -8,18 +8,18 @@ func NewCurrentState(n *Hn) *Element {
 
 		n.H2("Consultant track working hours"),
 
-		P(`Preferits consultants use excel file for gathering the
-	    hours and reporting them to the CEO. The file is prepared in
-	    advance by the CEO each year, with expected number of working
-	    hours for each day. The file contains monthly sheets and a
-	    yearly summary. Consultants register a project per line and
-	    enter number of hours for each day. There are also rows for
-	    vacation, illness and other non project specific tasks that
-	    may need time tracking.`),
+		P(`Preferits consultants use spreadsheet file for gathering
+	    the hours and reporting them to the CEO. The file is prepared
+	    in advance by the CEO each year, with expected number of
+	    working hours for each day. The file contains monthly sheets
+	    and a yearly summary. Consultants register a project per line
+	    and enter number of hours for each day. There are also rows
+	    for vacation, illness and other non project specific tasks
+	    that may need time tracking.`),
 
-		n.H2("CEO prepares excel file for consultant"),
+		n.H2("CEO prepares spreadsheet file for consultant"),
 
-		P(`Each year the CEO creates a new excel file based on a
+		P(`Each year the CEO creates a new spreadsheet file based on a
 		previous one. He goes through each month and verifies that the
 		calendar is correct and changes the expected working hours for
 		holidays. Once this is done the file is duplicated for each
@@ -29,17 +29,17 @@ func NewCurrentState(n *Hn) *Element {
 
 		n.H2("Consultant reports working hours to CEO"),
 
-		P(`Consultant e-mails the excel file to the CEO`),
+		P(`Consultant e-mails the spreadsheet file to the CEO`),
 
 		n.H2("Consultant reports working hours to contractor"),
 
-		P(`Consultant opens the excel file to find the summary for a
+		P(`Consultant opens the spreadsheet file to find the summary for a
 		specific project and e-mails that summary to the contractor.`),
 
 		n.H2("CEO creates invoice for customer"),
 
-		P(`CEO opens excelfile for consultant that is contracted and
-		finds the summary for the specific project.`,
+		P(`CEO opens spreadsheet file for consultant that is
+		contracted and finds the summary for the specific project.`,
 
 			Question(`What do you do with the summary? enter it into
 		    an invocing system, which?`),
